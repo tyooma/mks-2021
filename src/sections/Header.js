2f96b560c9685084a4053c01a55bb047d2b0b634
tyooma <flying.alien.godzilla@gmail.com>
2021-02-02 00:54:56 +0200

    change SESAM & GOLDENFEST description, remove cursor pointer from curator's photos, change second video link

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -36,7 +36,7 @@ export const Header = () => {
           className='header__video'
         />
         <iframe
-          src='https://www.youtube.com/embed/lrNKjRbLUF0'
+          src='https://www.youtube.com/embed/1VRsKeL0Jh0'
           frameborder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
           allowfullscreen
