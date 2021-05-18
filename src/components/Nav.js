c78e63a1a70b76dff6631a5e56f5a1591fa031b3
artem@buildmidwestern.com <artem@buildmidwewstern.com>
2021-05-18 13:53:47 +0300

    Update links and info blocks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,32 @@ export const Nav = () => (
   <nav className='nav'>
     <ul className='nav__list'>
       <li className='nav__item'>
-        <a href='https://www.facebook.com/CultureSlavutich' target='_blank' rel='noreferrer' className='nav__link'>
+        <a
+          href='https://www.facebook.com/CultureSlavutich'
+          target='_blank'
+          rel='noreferrer'
+          className='nav__link'
+        >
           Відділ культури Славутича
         </a>
       </li>
       <li className='nav__item'>
-        <a href='https://www.facebook.com/visitslavutych' target='_blank' rel='noreferrer' className='nav__link'>
+        <a
+          href='https://www.facebook.com/102391795005758/posts/239257054652564'
+          target='_blank'
+          rel='noreferrer'
+          className='nav__link'
+        >
+          Прес-анонси
+        </a>
+      </li>
+      <li className='nav__item'>
+        <a
+          href='https://www.facebook.com/visitslavutych'
+          target='_blank'
+          rel='noreferrer'
+          className='nav__link'
+        >
           Туристично-інформаційний центр
         </a>
       </li>
